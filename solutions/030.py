a51a049d8c3a9bc0dbda1b75890abb50b0786f61
arielmmoreira <arielmoraismoreira@gmail.com>
2023-09-16 18:41:32 -0300

    solution problem 030

---
+++ solutions/030.py
@@ -0,0 +1,26 @@
+"""
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+
+    1634 = 1^4 + 6^4 + 3^4 + 4^4
+    8208 = 8^4 + 2^4 + 0^4 + 8^4
+    9474 = 9^4 + 4^4 + 7^4 + 4^4
+
+As 1 = 1^4 is not a sum it is not included.
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+"""
+
+n = 5
+result = 0
+for i in range(2, 999999):
+    current_number = i
+    cum_sum = 0
+    while current_number > 0:
+        cum_sum += (current_number % 10) ** n
+        current_number = current_number // 10
+
+    if cum_sum == i:
+        result += cum_sum
+
+
+print('Answer:', result)
